c0039a36c01e02a38387c40dd309bc4c4f183c27
Jonathan Jan (Orange Lab) <jmj2097@rit.edu>
2023-03-03 14:34:57 -0500

    added a general solution so that we can have multiple projects in the project

---
+++ RailGun/Editor/Controls/SampleControl.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Forms.Controls;
+
+namespace MonoGame.Forms.DX.Controls
+{
+    public class SampleControl : MonoGameControl
+    {
+        const string WelcomeMessage = "Hello MonoGame.Forms!";
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw()
+        {
+            base.Draw();
+
+            Editor.spriteBatch.Begin();
+
+            Editor.spriteBatch.DrawString(Editor.Font, WelcomeMessage, new Vector2(
+                (Editor.graphics.Viewport.Width / 2) - (Editor.Font.MeasureString(WelcomeMessage).X / 2),
+                (Editor.graphics.Viewport.Height / 2) - (Editor.FontHeight / 2)),
+                Color.White);
+
+            Editor.spriteBatch.End();
+        }
+    }
+}
---
+++ RailGun/Editor/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MonoGame.Forms.DX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sampleControl = new MonoGame.Forms.DX.Controls.SampleControl();
+            this.SuspendLayout();
+            // 
+            // sampleControl
+            // 
+            this.sampleControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sampleControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sampleControl.Location = new System.Drawing.Point(0, 0);
+            this.sampleControl.MouseHoverUpdatesOnly = false;
+            this.sampleControl.Name = "sampleControl";
+            this.sampleControl.Size = new System.Drawing.Size(582, 353);
+            this.sampleControl.TabIndex = 0;
+            this.sampleControl.Text = "Sample Control";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.sampleControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.SampleControl sampleControl;
+    }
+}
+
