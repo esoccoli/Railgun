f021bcef0285ee2252c51dfdac3b5ef1683da1f5
Jonathan Jan (Laptop) <jmj2097@rit.edu>
2023-04-21 16:03:00 -0400

    added entity images and entity class

---
+++ Railgun/Editor/App/Objects/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Railgun.Editor.App.Objects
+{
+    /// <summary>
+    /// Represents an entity that can be placed in the editor
+    /// <para>Author: Jonathan Jan</para>
+    /// Date Created: 4/21/2023
+    /// </summary>
+    internal class Entity
+    {
+
+    }
+}
